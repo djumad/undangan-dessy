621bbdab4f848df66fd86b2becae083c4869d725
djumad <bantandjumad@gmail.com>
2024-12-27 13:08:57 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from 'svelte';
+  import foto from "./assets/foto.jpg";
+  let nama = "Dessy Grecya Gultom";
+  let acara = "Wisuda Politeknik Negeri Ambon";
+  let tanggal = "28 Desember 2024";
+  let waktu = "10:00 WIT";
+  let lokasi = "Waitatiri";
+  let penyelenggara = "Politeknik Negeri Ambon";
+
+  import Router from './lib/Router.svelte';
+  onMount(() => {
+    // kode untuk mengatur tampilan saat komponen di-mount
+  });
+</script>
+<Router />
---
+++ src/lib/Router.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {Router , Route} from "svelte-routing";
+    import To from "../page/To.svelte";
+    import Home from "../page/Home.svelte";
+let url = $state("");
+</script>
+
+<Router {url}>
+    <Route path="/:nama" let:params>
+        <To nama="{params.nama}" />
+    </Route>
+    <Route path="/" component={Home} />
+</Router>
+
